f56ea27d85557cecfc0f8ccde701df0150f83c94
avivansh <vanshmittal1299@gmail.com>
2021-12-11 14:21:02 +0530

    cleanup

--- learning-react/contact-list/src/App.js
+++ learning-react/contact-list/src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learnin react from youtube.
         </a>
       </header>
     </div>
